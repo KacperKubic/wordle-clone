1ae821ae325510706045074e223d76db2e324b6d
KacperKubic <kacper.kubic2211@gmail.com>
2023-03-06 13:12:03 +0100

    Testing what causes error on vercel deployment

--- src/Data.js
+++ src/Data.js
@@ -13,7 +13,8 @@ export const generateWordSet = async () => {
   let wordSet;
   let todayWord;
   await fetch(wordBank).then((response) => response.text()).then((result) => {
-    const wordArr = result.split("\r")
+    console.log(wordBank)
+    const wordArr = result.split("\n")
     console.log(wordArr)
     todayWord = wordArr[Math.floor(Math.random() * wordArr.length)]
     console.log(todayWord)
